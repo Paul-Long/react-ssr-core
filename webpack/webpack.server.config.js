43d365da91b0ce600aab9244b447c2178124ed66
候宝龙 <baolong.hou@sumscope.com>
2018-09-27 14:08:00 +0800

    fixed server render table

--- webpack/webpack.server.config.js
+++ webpack/webpack.server.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 const root = path.resolve(__dirname, '../');
@@ -9,7 +8,9 @@ const env = process.env.NODE_ENV;
 const config = {
   entry: path.resolve(root, 'src/client/render-server.js'),
   target: 'node',
-  externals: [nodeExternals()],
+  externals: [nodeExternals({
+    whitelist: ['fast-table']
+  })],
   output: {
     libraryTarget: 'commonjs2',
     filename: 'bundle_server.js',
@@ -31,9 +32,8 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         use: ['babel-loader'],
-        exclude: path.resolve(root, 'node_modules'),
       },
       {
         test: /\.(css|less)/,
@@ -41,7 +41,6 @@ const config = {
       },
     ]
   },
-  devtool: 'source-map',
   plugins: [],
 };
 
@@ -49,10 +48,10 @@ config.mode = env;
 
 if (env === 'development') {
   config.devtool = 'source-map';
-  // config.plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
 if (env === 'production') {
-
+  // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+  // config.plugins.push(new BundleAnalyzerPlugin());
 }
 module.exports = config;
