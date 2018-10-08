e197813923d0ddab3362cc72aabdda81c7dbce82
候宝龙 <baolong.hou@sumscope.com>
2018-10-08 11:28:20 +0800

    u

--- bin/start.sh
+++ bin/start.sh
@@ -6,25 +6,5 @@ echo "start npm install"
 npm i
 echo "npm install finished"
 
-echo "clean dist"
-npm run clean
-echo "clean finished"
-
-echo "build react"
-npm run build:react
-echo "build react finished"
-
-echo "build echarts"
-npm run build:echarts
-echo "build echarts finished"
-
-echo "build client"
-npm run build:client
-echo "build client finished"
-
-echo "build server"
-npm run build:server
-echo "build server finished"
-
 echo "start app"
 pm2 start index.js --name ssr
